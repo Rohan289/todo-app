1a55337fc24b33dc86daa504d3a623e2533e4556
Rohan Bhowmick <rohanbhowmick@Rohans-MacBook-Air.local>
2024-11-07 12:57:40 +0530

    Added env for postgresql attributes

--- src/typeorm/datasource.ts
+++ src/typeorm/datasource.ts
@@ -3,14 +3,20 @@ import { Todo } from '@/models/Todo';
 import { User } from '@/models/User';
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {DATABASE_NAME,DATABASE_HOST,DATABASE_PORT,DATABASE_USER,DATABASE_PASSWORD} = process.env;
+
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',  // replace with your PostgreSQL username
-  password: '1032',  // replace with your PostgreSQL password
-  database: 'collaborative-todo',  // replace with your PostgreSQL database name
+  host: DATABASE_HOST,
+  port: parseInt(DATABASE_PORT as string),
+  username: DATABASE_USER,  // replace with your PostgreSQL username
+  password: DATABASE_PASSWORD,  // replace with your PostgreSQL password
+  database: DATABASE_NAME,  // replace with your PostgreSQL database name
   synchronize: true, // auto-sync entity schema with the database
   logging: false,
   entities: [Todo,User],
