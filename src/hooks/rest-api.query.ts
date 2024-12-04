485645a7417cd75e55e16467c27cdaae3fffa104
Rohan Bhowmick <rohanbhowmick@Rohans-MacBook-Air.local>
2024-12-04 18:25:20 +0530

    Updated create todo modal for epics and stories

--- src/hooks/rest-api.query.ts
+++ src/hooks/rest-api.query.ts
@@ -1,4 +1,5 @@
 // hooks/useTodos.ts
+import { TodoTaskType, TodoSubTaskType } from '@/app/ui/todoCard/TodoCard.model';
 import { useQuery } from '@tanstack/react-query';
 
 const fetchTodos: (queryString : string,pathParam: string) => Promise<unknown> = async (queryString : string,pathParam: string) => {
@@ -31,6 +32,21 @@ const fetchStories: (queryString : string,pathParam: string) => Promise<unknown>
     return data.data; // Adjust based on your API response structure
 };
 
+const fetchEpics: (queryString : string,pathParam: string) => Promise<unknown> = async (queryString : string,pathParam: string) => {
+
+    const url = `/api/epic${pathParam ? `/${pathParam}` : queryString ? `?${queryString}` : ''}`;
+    const response = await fetch(url, {
+        cache: 'no-store',
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch todos');
+    }
+
+    const data = await response.json();
+    return data.data; // Adjust based on your API response structure
+};
+
 const fetchUsers: () => Promise<unknown> = async () => {
 
     const response = await fetch('/api/user', {
@@ -48,6 +64,16 @@ const fetchUsers: () => Promise<unknown> = async () => {
 interface UseTodosParams {
     queryString?: string;
     pathParam?: string;
+    type?: TodoTaskType;
+    subType?:TodoSubTaskType;
+}
+
+interface UseTasksParams {
+    enableQuery : boolean;
+    queryString?: string;
+    pathParam?: string;
+    type?: TodoTaskType;
+    subType?:TodoSubTaskType;
 }
 
 export const useTodos = <T>({ queryString = '', pathParam = '' }: UseTodosParams = {}) => {
@@ -57,10 +83,31 @@ export const useTodos = <T>({ queryString = '', pathParam = '' }: UseTodosParams
     });
 };
 
-export const useStories = <T>({ queryString = '', pathParam = '' }: UseTodosParams = {}) => {
-    return useQuery<T, Error, T, [string, string, string]>({
+export const useStories = <T>({ enableQuery = false, queryString = '', pathParam = '' }: UseTasksParams, onSuccess?:() => void) => {
+    const options = {
+        enabled : enableQuery,
+        onSuccess : () => {
+            onSuccess?.();
+        }
+    }
+    return useQuery({
         queryKey: ['stories', queryString, pathParam],
         queryFn: () => fetchStories(queryString, pathParam) as Promise<T>,
+        ...options
+    });
+};
+
+export const useEpics = <T>({ enableQuery = false, queryString = '', pathParam = '' }: UseTasksParams, onSuccess?:() => void) => {
+    const options = {
+        enabled : enableQuery,
+        onSuccess : () => {
+            onSuccess?.();
+        }
+    }
+    return useQuery({
+        queryKey: ['epics', queryString, pathParam],
+        queryFn: () => fetchEpics(queryString, pathParam) as Promise<T>,
+        ...options
     });
 };
 
